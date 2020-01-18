3d3b83cd220a4f1c3738d7d8eb47ff8385f795dd
Laura Wiseman <laurawiseman@dhcp-10-105-55-105.wireless.northwestern.private>
2020-01-18 16:50:22 -0600

    isolated Arizona city names, cleaned lettering, missing entries, and directional extraneous info

--- part2.py
+++ part2.py
@@ -9,12 +9,19 @@ if __name__ == '__main__':
     for l in open(path + 'business.json').readlines():
         d.append(json.loads(l))  # from json str to dict
     df = pd.DataFrame.from_records(d)
-    daz = pd.Series([x.city] for x in df.state[AZ])
-    print(daz)
-    
+    # daz = pd.Series([x.city] for x in df.state["AZ"])
+    # daz = pd.Series([x.city] for x in df.query('state == ["AZ"]'))
+    daz = df.query('state == ["AZ"]').city
+    daz = daz.str.upper()  # account for different lettering
+    daz = daz.dropna(axis=0)  # account for missing entries
+    daz = daz.str.replace('NORTH|SOUTH|EAST|WEST', '', case=False).str.strip()  # account for directions and whitespace
+
+    # print(daz[0:10])
+    print(len(daz))
+    print(len(daz.unique()))
+
 #     da = []
 #     for business in df:
-#       if df.state[business] == "AZ": 
+#       if df.state[business] == "AZ":
 #         da.append(df.city[business]
-#     print(da)
-    
+#     print(daz)
