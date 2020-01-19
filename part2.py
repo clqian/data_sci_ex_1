7189c94bcd16a33d6950eba112bc1616f9f2e395
Laura Wiseman <laurawiseman@Lauras-MacBook.local>
2020-01-19 11:14:59 -0600

    cleaned up code

--- part2.py
+++ part2.py
@@ -27,27 +27,16 @@ if __name__ == '__main__':
     for l in open(path + 'business.json').readlines():
         d.append(json.loads(l))  # from json str to dict
     df = pd.DataFrame.from_records(d)
-    # daz = pd.Series([x.city] for x in df.state["AZ"])
-    # daz = pd.Series([x.city] for x in df.query('state == ["AZ"]'))
     daz = df.query('state == ["AZ"]').city
     daz = daz.str.upper()  # account for different lettering
     daz = daz.dropna(axis=0)  # account for missing entries
     daz = daz.str.replace('NORTH|SOUTH|EAST|WEST', '', case=False).str.strip()  # account for directions and whitespace
 
-    daz = daz.apply(lambda x: check_typos(x))
-
-    # print(daz[0:10])
-    # print(len(daz))
-    # print(len(daz.unique()))
-
-#     da = []
-#     for business in df:
-#       if df.state[business] == "AZ":
-#         da.append(df.city[business]
-#     print(daz)
+    daz = daz.apply(lambda x: check_typos(x))   # account for any typos and change invalid cities to ''
 
     df.update(daz)
 
+    # account for any invalid city names
     indices = df[(df.city == 'AZ') | (df.city == 'Arizona') | ((df.state == 'AZ') & (df.city == ''))].index
     df.drop(indices, inplace=True)
 
